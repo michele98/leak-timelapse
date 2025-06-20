2a8e321f3f91f52e75bae0db5c9707777531c2a1
michele98 <calvanese.michele@gmail.com>
2025-06-21 00:44:44 +0200

    add new picture

--- main.py
+++ main.py
@@ -14,7 +14,8 @@ FRAMES_LIST_VIDEO = [
     "20250616_195033.jpg",
     "20250617_193733.jpg",
     "20250618_184236.jpg",
-    "20250619_194322.jpg"
+    "20250619_194322.jpg",
+    "20250620_205703.jpg"
 ] # Subset of frames used for video creation
 
 
